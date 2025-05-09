08c8e6841e3a993b2bff68644bc7151a718c5be0
Juan Bautista Talens Felis <43112835+juatafe@users.noreply.github.com>
2025-05-09 14:48:57 +0200

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Importador d'usuaris del portal",
+    "summary": "Importa usuaris del portal des d'un fitxer Excel i els vincula amb contactes existents",
+    "version": "1.0",
+    "depends": ["base"],
+    "data": [
+        "views/import_users_wizard_view.xml"
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3"
+}
